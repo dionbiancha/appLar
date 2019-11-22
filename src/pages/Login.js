dce3c551db4d47f7b6f7e473d5fc173e04b00274
Dionei Bianchati <dion.biancha@gmail.com>
2019-11-22 08:19:02 -0300

    End Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { 
+    View, 
+    StyleSheet,
+    TouchableOpacity,
+    Text ,
+    Image
+} from 'react-native';
 
-import LoginButton from '../components/login/LoginButon';
 
+export default class Login extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={styles.textArea}>
+          <Image source={require('../img/logo.png')} style={styles.logo} />
+          <Text style={styles.textLogo}>A gente ama ver </Text>
+          <Text style={styles.textLogo}>
+            você 
+            <Text style={styles.textBig}> Feliz</Text>
+          </Text>
+        </View>
+        <View style={styles.loginArea}>
+          <Text style={styles.textLogin}>Para acessar é necessario estar logado</Text>
+            <TouchableOpacity style={[styles.button, {backgroundColor: '#355495'}]}>
+              <Image source={require('../icons/iconFacebook.png')} style={styles.icon}/>
+              <Text style={styles.textButton}>Entrar com o Facebook</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, {backgroundColor: '#D74635'}]}>
+              <Image source={require('../icons/iconGoogle.png')} style={styles.icon} />
+              <Text style={styles.textButton}>Entrar com o Google</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
-const Login: () => React$Node = () => {
-  return (
-    <>
-      <LoginButton />
-    </>
-  );
-};
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 50,
+      marginBottom: 50
+    },
+    loginArea: {
+      height: 130,
+      margin: 10
+    },
+    textLogin: {
+      color: '#000000',
+      textAlign: 'center',
+      marginBottom: 15
+    },
+    button: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      height: 50,
+      borderRadius: 5,
+      margin: 5
+    },
+    icon: {
+      position: 'absolute',
+      width: 25,
+      height: 25
 
-export default Login;
+    },
+    textButton: {
+      flex: 1,
+      justifyContent: 'center',
+      textAlign: 'center',
+      color: '#FFFFFF',
+      fontWeight: 'bold'
+    },
+    textArea: {
+      flex: 1,
+      alignItems: 'center',
+      flexDirection: 'column'
+    },
+    logo: {
+      width: 250,
+      height: 200
+    },
+    textLogo: {
+      fontSize: 25
+    },
+    textBig: {
+      color: '#F7213D', 
+      fontSize: 35,
+      fontWeight: 'bold'
+    }
+});

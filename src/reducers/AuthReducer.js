c28b718cb61cfdf67c65989b8be2182017fb99bf
Dionei Bianchati <dion.biancha@gmail.com>
2020-01-28 22:11:44 -0300

    Redux Structure

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    name: ''
+};
+
+const AuthReducer = (state = initialState, action) => {
+    return state;
+};
+
+export default AuthReducer;
--- src/routes/stackNavigator.js
+++ src/routes/stackNavigator.js
@@ -1,5 +1,11 @@
+import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+import Reducers from '../Reducers';
 
 import Login from '../pages/Login';
 import Tutorial from '../pages/Tutorial';
@@ -7,7 +13,9 @@ import TabNavigator from './tabNavigator';
 import {Churrascometro, ChurrascometroProdutos} from '../pages/churrascometro';
 import Product from '../pages/Product';
 
-const StackNavigator = createAppContainer(
+let store = createStore(Reducers, applyMiddleware(ReduxThunk));
+
+const Stack = createAppContainer(
   createStackNavigator({
     Login: {
       screen: Login,
@@ -36,5 +44,13 @@ const StackNavigator = createAppContainer(
   })
 );
 
-export default StackNavigator; 
+export default class Container extends Component {
+  render() {
+    return(
+      <Provider store={store}>
+        <Stack />
+      </Provider>
+    );
+  }
+}
 

c2a3afd787dbf8729351f8dd7b76f68d7aa26d22
dionbiancha <dion.biancha@gmail.com>
2020-02-02 23:16:35 -0300

    Modal add list

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Container from './src/routes/stackNavigator';
 
+console.disableYellowBox = true;
+
 const App = () => <Container />
 
 export default App; 
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
 import {
-    View,
-    Text,
-    Image,
+	View,
+	Modal,
+	Text,
+	TextInput,
+	Button,
+	Image,
+	TouchableOpacity,
     StyleSheet
 } from 'react-native';
 
 import TopMain from '../components/header/topMain';
 import CustomList from '../components/field/CustomList';
+import SimpleButton from '../components/buttons/simpleButton';
 
 export default class List extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalVisible: false
+		};
+
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	};
+
     static navigationOptions = {
 		tabBarIcon: ({ focused, tintColor }) => {
 		    if (focused) {
@@ -23,13 +39,48 @@ export default class List extends Component {
 		    }
 		}
 	}
+
+	openModal() {
+		let s = this.state;
+		s.modalVisible = true;
+		this.setState(s);
+	}
+
+	closeModal() {
+		let s = this.state;
+		s.modalVisible = false;
+		this.setState(s);
+	}
+
     render(){
         return(
             <View style={styles.container}>
 				<View style={styles.topMain}>
 					<TopMain name=""/>
+					<TouchableOpacity onPress={this.openModal}>
+						<Image style={styles.iconMore} source={require('../icons/moreGray.png')}/>
+					</TouchableOpacity>
 				</View>  
 				<CustomList />
+				<Modal animationType="fade" visible={this.state.modalVisible}>
+					<View style={styles.modal} > 
+						<Text style={styles.textTop}>Criar nova lista</Text>
+						<View>
+							<Text style={styles.textInput}>Nome</Text>
+							<TextInput style={styles.inputArea}/>
+							<Text style={styles.textInput}>Descrição</Text>
+							<TextInput style={styles.inputArea}/>
+						</View>
+						<View style={styles.buttonArea}>
+							<View style={{margin:10}}>
+								<SimpleButton bg="#3F51B5" text="Criar" />
+							</View>
+							<View style={{margin:10}}>
+								<SimpleButton bg="#EC6D79" text="Cancelar" onPress={this.closeModal} />
+							</View>
+						</View>	
+					</View>
+				</Modal>
             </View>
         );
     }
@@ -45,6 +96,38 @@ const styles = StyleSheet.create({
 		flexDirection: 'column'
 	},
 	topMain: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
 		margin: 10
+	},
+	iconMore: {
+		width: 35,
+		height: 35,
+		margin: 10
+	},
+	modal: {
+		flex: 1,
+		justifyContent: 'space-between'
+	},
+	textTop: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		margin: 10,
+		color: '#757575'
+	},
+	textInput: {
+		fontSize: 20,
+		marginLeft: 10
+	},
+	inputArea: {
+		height: 40,
+		borderBottomWidth: 1,
+		margin: 10,
+		borderColor: '#757575'
+	},
+	buttonArea: {
+		justifyContent: 'center',
+		flexDirection: 'row'
 	}
 });
